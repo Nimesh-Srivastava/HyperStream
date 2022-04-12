66e63a595b5b4f1163f01a3776b16cfce5f20c64
Nimesh Srivastava <nimeshsrivastava00@gmail.com>
2022-04-12 16:23:22 +0530

    App initialised

--- App.tsx
+++ App.tsx
@@ -1,22 +1,28 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import useCachedResources from './src/hooks/useCachedResources';
+import useColorScheme from './src/hooks/useColorScheme';
+import Navigation from './src/navigation';
+
+import { StreamChat } from 'stream-chat';
+
+const API_KEY = "xmcvcbgkcwu8";
+const client = StreamChat.getInstance(API_KEY);
 
 export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
+	const isLoadingComplete = useCachedResources();
+	const colorScheme = useColorScheme();
 
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
-    );
-  }
+	if (!isLoadingComplete) {
+		return null;
+	} else {
+    	return (
+    		<SafeAreaProvider>
+				<Navigation colorScheme={colorScheme} />
+        		<StatusBar />
+    	  	</SafeAreaProvider>
+		);
+	}
 }
